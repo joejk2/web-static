71a449907e2f070760e15efdfa3ad5b2924668ec
Joe Kwiatkowski <joejk2@gmail.com>
2024-08-19 12:02:25 +0100

    Add cdn-collate

---
+++ scripts/cdn-collate.sh
@@ -0,0 +1,29 @@
+################################################################################
+## Collate assets to be uploaded to CDN
+##
+## Paths are renamed to reflect those used when serving the site locally
+##
+## Usage
+##   source cdn-collate.sh  (run from /web-static)
+################################################################################
+
+## CDN PATH RELATIVE TO ROOT OF WEB-STATIC
+CDN_PATH='../cdn'
+CDN_PATH_ESC='..\/cdn'
+
+## FIND FILES
+find . -path "./content/*/assets/*" -o -path "./assets/img/*" > /tmp/cdn-index
+
+## CLEAN CDN
+#rm -rf ${CDN_PATH}/*
+
+## COPY FILES
+for source in $(cat /tmp/cdn-index);
+do
+  destination=$(echo $source | \
+    sed -e "s/\.\//${CDN_PATH_ESC}\//" \
+    -e "s/content\/_//")
+  mkdir -p $(dirname $destination)
+  echo "cp $source $destination"
+  cp $source $destination
+done;
